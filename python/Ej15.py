291975432e412d43bdf0b8a1d94b7870376871fb
smr1-Jaime <116664634+smr1-Jaime@users.noreply.github.com>
2023-06-08 13:05:17 +0200

    Actualizados Ej 15 y 17

--- python/Ej15.py
+++ python/Ej15.py
@@ -1,5 +1,6 @@
 #Dada una secuencia de números leídos por teclado, que acabe con un –1, por ejemplo: 5,3,0,2,4,4,0,0,2,3,6,0,……,-1;
 #Realizar el algoritmo que calcule la media aritmética. Suponemos que el usuario no insertará números negativos.
+
 numeros = input('Introduce los números:')
 numeros = numeros.split(',')
 numeros = [float(numero) for numero in numeros]
--- python/Ej17.py
+++ python/Ej17.py
@@ -1,4 +1,14 @@
 #Algoritmo que lea números enteros hasta teclear 0, y nos muestre el máximo, el mínimo y la media de todos ellos.
 #Piensa como debemos inicializar las variables.
 
-print(max(1, 23, 3, 4, 5, 65))
+numeros = input('Introduzca numeros:')
+numeros_div = numeros.split()
+numeros = [float(numero) for numero in numeros_div]
+
+maximo = max(numeros)
+minimo = min(numeros)
+media = sum(numeros) / len(numeros)
+
+print(maximo)
+print(minimo)
+print(media)
