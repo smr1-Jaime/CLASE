4c2e5713556a003086eb95c96c9029d19331376a
smr1-Jaime <116664634+smr1-Jaime@users.noreply.github.com>
2023-06-08 13:41:28 +0200

    Update Ej19 ACABAR.py

---
+++ python/Ej19 ACABAR.py
@@ -0,0 +1,5 @@
+#Leer tres números que denoten una fecha (día, mes, año). Comprobar que es una fecha válida.
+#Si no es válida escribir un mensaje de error. Si es válida escribir la fecha cambiando el número del mes por su nombre.
+#Ej. Si se introduce 1 2 2006, se deberá imprimir “1 de febrero de 2006”. El año debe ser mayor que 0.
+#(Suponemos que una fecha es correcta si el día está entre 1 y 31, el mes entre 1 y 12 y el año es mayor de cero)
+
