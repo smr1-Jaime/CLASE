ceeabc0b96002ff8ae7184686addc73f4e4fcb62
smr1-Jaime <116664634+smr1-Jaime@users.noreply.github.com>
2023-06-09 12:28:02 +0200

    Finalizado Ej22.py

--- python/Ej22.py
+++ python/Ej22.py
@@ -1,6 +1,8 @@
 #Imprimir y contar los números que son múltiplos de 2 o de 3 que hay entre 1 y 100.
-def contador():
-    for i in range(1, 101):
-        if i % 2 == 0 or i % 3 == 0:
-            lista = i
-            print(lista)
+contador = 0
+for i in range(1, 101):
+    if i % 2 == 0 or i % 3 == 0:
+        print(i)
+        contador +=1
+        
+print(f'El total de numeros multiplos de 2 o 3 es: {contador}')
