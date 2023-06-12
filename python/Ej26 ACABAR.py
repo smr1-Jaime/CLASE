0a885756401d2f62501aa7d940f85f1c7bbaa42b
smr1-Jaime <116664634+smr1-Jaime@users.noreply.github.com>
2023-06-12 10:24:41 +0200

    Finalizado
    Finalizado Ej26.py

--- python/Ej26 ACABAR.py
+++ python/Ej26 ACABAR.py
@@ -1,3 +1,15 @@
 #Comprobar si un número, mayor o igual que la unidad, es primo.
 
-num = int(input('Introduzca un numero: \r\n'))
+numero = int(input('Introduzca un numero: \r\n'))
+
+if numero >= 1:
+    divisores = 0
+    for divisor in range(1, numero + 1):
+        if (numero % divisor) == 0:
+            divisores += 1
+    if divisores == 2:
+        print(f'{numero} es primo')
+    else:
+        print(f'{numero} no es primo')
+else:
+    print(f'ERROR. El numero introducido debe ser mayor o igual que 1')
