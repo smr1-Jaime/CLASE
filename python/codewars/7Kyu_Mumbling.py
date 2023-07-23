bf3b3382997b14cf9040b7a2da9150bfe2eea2c6
smr1-Jaime <moranchel.jaime.smr@gmail.com>
2023-07-23 09:41:54 +0200

    Added exercises

--- python/codewars/7Kyu_Mumbling.py
+++ python/codewars/7Kyu_Mumbling.py
@@ -5,13 +5,18 @@
     # accum("cwAt") -> "C-Ww-Aaa-Tttt"
 # The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-def accum(string):
-    multiplicador = 1
-    string2 = ""
-    for i in string:
-        string2 += (i*multiplicador)
-        string2 += "-"
-        multiplicador += 1
-    return(string2[:-1])
+# def accum(string):
+#     multiplicador = 1
+#     string2 = ""
+#     for i in string:
+#         string2 += (i*multiplicador)
+#         string2 += "-"
+#         multiplicador += 1
+#     return(string2[:-1])
 
-print(accum("patata"))
+def accum(s):
+    result = []
+    for i, char in enumerate(s, 1):
+        result.append((char*i).title())
+    return('-'.join(result))
+print(accum("paTata"))
